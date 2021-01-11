18c52ef17988c0c4524180564bed32f5d7874b50
Marvin Drees <marvin@ceres-sys.de>
2021-01-11 15:53:43 +0100

    Implement proper signal handling and move GetReviewers code out of MessageCreate function

--- bot.go
+++ bot.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func SessionCreate(tok string) *discordgo.Session {
@@ -20,15 +23,18 @@ func StartBot(a *API, bot *discordgo.Session) {
 	// Register events
 	bot.AddHandler(Ready)
 	bot.AddHandler(MessageCreate)
-	
+
 	err := bot.Open()
 	if err != nil {
 		fmt.Println("Error opening Discord session: ", err)
 	}
+
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
-	for {
-	}
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
 	// Cleanly close down the Discord session.
 	bot.Close()
 }
@@ -40,15 +46,7 @@ func Ready(s *discordgo.Session, event *discordgo.Ready) {
 func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if m.Content == "!reviewers" {
-		response, err := a.GetReviewerRequest()
-		if err != nil {
-			panic(err)
-		}
-		if response == nil {
-			s.ChannelMessageSend(m.ChannelID, "Empty Response")
-		} else {
-			s.ChannelMessageSend(m.ChannelID, string(len(response.Reviewer)))
-		}
+		s.ChannelMessageSend(m.ChannelID, GetReviewers()) // TODO: Implement Me
 	}
 
 	// Just for testing purpose
